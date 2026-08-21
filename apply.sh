5a464c26a98e2ef442137221f1f4fcc21a818716
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-08-21 14:40:39 +0200

    reduce logging

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 # Tip: Start with a blank arch
 
 # === YAY ===
-sudo pacman -S --needed git base-devel --noconfirm
+sudo pacman -S --needed git base-devel --noconfirm --quiet
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -9,61 +9,61 @@ cd ..
 rm -rf yay
 
 # === HYPRLAND ===
-sudo pacman -S hyprland --noconfirm
+sudo pacman -S hyprland --noconfirm --quiet
 mkdir -p ~/.config/hypr
 cp .config/hypr/hyprland.lua ~/.config/hypr/hyprland.lua
 
 # === KITTY ===
-sudo pacman -S kitty --noconfirm
+sudo pacman -S kitty --noconfirm --quiet
 mkdir -p ~/.config/kitty
 cp .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 # === FISH ===
-sudo pacman -S fish --noconfirm
+sudo pacman -S fish --noconfirm --quiet
 mkdir -p ~/.config/fish
 cp .config/fish/config.fish ~/.config/fish/config.fish
 # Make default shell
 chsh -s "$(command -v fish)"
 
 # === NEOVIM ===
-sudo pacman -S neovim --noconfirm
+sudo pacman -S neovim --noconfirm --quiet
 mkdir -p ~/.config/nvim
 cp .config/nvim/init.lua ~/.config/nvim/init.lua
 # Lua Autocompletes
-sudo pacman -S lua-language-server --noconfirm
+sudo pacman -S lua-language-server --noconfirm --quiet
 # QMLJS Autocompletes (https://quickshell.org/docs/v0.3.0/guide/install-setup/#:~:text=Neovim%20has)
 nvim --headless "+TSInstall qmljs" +qa
 
 # === STARSHIP ===
-sudo pacman -S starship --noconfirm
+sudo pacman -S starship --noconfirm --quiet
 cp .config/starship.toml ~/.config/starship.toml
 
 # === SUPERFILE ===
-sudo pacman -S superfile --noconfirm
+sudo pacman -S superfile --noconfirm --quiet
 
 # === HYPRPAPER ===
-sudo pacman -S hyprpaper --noconfirm
+sudo pacman -S hyprpaper --noconfirm --quiet
 mkdir -p ~/.config/hypr/
 cp .config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 mkdir -p ~/Ricing/Wallpapers
 cp Ricing/Wallpapers/Cedeira.jpg ~/Ricing/Wallpapers/Cedeira.jpg
 
 # === QUTEBROWSER ===
-sudo pacman -S qutebrowser --noconfirm
+sudo pacman -S qutebrowser --noconfirm --quiet
 mkdir -p ~/.config/qutebrowser/userscripts
 cp .config/qutebrowser/userscripts/* ~/.config/qutebrowser/userscripts
 
 # === FASTFETCH ===
-sudo pacman -S fastfetch --noconfirm
+sudo pacman -S fastfetch --noconfirm --quiet
 mkdir -p ~/.config/fastfetch
 cp .config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 
 # === QUICKSHELL ===
-sudo pacman -S quickshell --noconfirm
+sudo pacman -S quickshell --noconfirm --quiet
 mkdir -p ~/.config/quickshell
 cp .config/quickshell/* ~/.config/quickshell/
 
 # === 1PASSWORD ===
 sudo pacman -S jq rofi-wayland wl-clipboard
-yay -S 1password-cli --noconfirm
+yay -S 1password-cli --noconfirm --quiet
 
