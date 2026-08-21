7024b67b813535f054e5e7f1de4917dd6f023e9e
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-08-21 02:47:21 +0200

    improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 end
 
 # Start Hyprland on TTY1
