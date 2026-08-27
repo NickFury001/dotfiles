cbd96436dc25fb3750a28a87ab80d0658bf5d8c6
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-08-27 22:24:28 +0200

    improvements

--- apply.sh
+++ apply.sh
@@ -24,7 +24,7 @@ if [[ "${1:-}" == "--debug" ]]; then
 fi
 
 # Set the total number of steps to calculate the percentage
-TOTAL_STEPS=13
+TOTAL_STEPS=14
 CURRENT_STEP=0
 
 CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
